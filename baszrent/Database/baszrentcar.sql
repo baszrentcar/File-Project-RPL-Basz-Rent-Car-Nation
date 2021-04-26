c6e27159c4af1e1faf41496d9aca5602a447eb2b
baszrentcar <80313322+baszrentcar@users.noreply.github.com>
2021-04-27 00:21:28 +0700

    Add files via upload

---
+++ baszrent/Database/baszrentcar.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 26, 2021 at 07:20 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `baszrentcar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `costumer`
+--
+
+CREATE TABLE `costumer` (
+  `no_ktp` varchar(16) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `costumer`
+--
+
+INSERT INTO `costumer` (`no_ktp`, `nama`, `email`, `username`, `password`) VALUES
+('0099088', 'qwe', 'qwe@gmail.com', 'qwe', '321'),
+('3123459876', 'ADMIN', 'admin@gmail.com', 'admin', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `historydatamobilyangdihapus`
+--
+
+CREATE TABLE `historydatamobilyangdihapus` (
+  `id_historymobil` int(4) NOT NULL,
+  `id_mobil` int(4) NOT NULL,
+  `tgl_hapus` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` int(4) NOT NULL,
+  `nama_mobil` varchar(20) NOT NULL,
+  `harga` int(8) NOT NULL,
+  `warna` varchar(10) NOT NULL,
+  `plat_nomor` varchar(10) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `deskripsi` varchar(50) NOT NULL,
+  `Penumpang` varchar(7) NOT NULL,
+  `Bagasi` varchar(15) NOT NULL,
+  `photo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(4) NOT NULL,
+  `id_pemesanan` int(4) NOT NULL,
+  `total_harga` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan`
+--
+
+CREATE TABLE `pemesanan` (
+  `id_pemesanan` int(4) NOT NULL,
+  `id_supir` int(4) NOT NULL,
+  `id_mobil` int(4) NOT NULL,
+  `no_ktp` varchar(16) NOT NULL,
+  `tanggal_pemesanan` int(10) NOT NULL,
+  `tanggal_kembali` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemilik_rental`
+--
+
+CREATE TABLE `pemilik_rental` (
+  `no_ktp` varchar(16) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supir`
+--
+
+CREATE TABLE `supir` (
+  `id_supir` int(4) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `kontak` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testimoni`
+--
+
+CREATE TABLE `testimoni` (
+  `id_testimoni` int(4) NOT NULL,
+  `no_ktp` varchar(16) NOT NULL,
+  `pesan` varchar(50) NOT NULL,
+  `rating` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `testimoni`
+--
+
+INSERT INTO `testimoni` (`id_testimoni`, `no_ktp`, `pesan`, `rating`) VALUES
+(3, 'dump', 'mantap', ''),
+(4, 'dump', 'mantap', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `costumer`
+--
+ALTER TABLE `costumer`
+  ADD PRIMARY KEY (`no_ktp`);
+
+--
+-- Indexes for table `historydatamobilyangdihapus`
+--
+ALTER TABLE `historydatamobilyangdihapus`
+  ADD PRIMARY KEY (`id_historymobil`);
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`);
+
+--
+-- Indexes for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD PRIMARY KEY (`id_pemesanan`);
+
+--
+-- Indexes for table `pemilik_rental`
+--
+ALTER TABLE `pemilik_rental`
+  ADD PRIMARY KEY (`no_ktp`);
+
+--
+-- Indexes for table `supir`
+--
+ALTER TABLE `supir`
+  ADD PRIMARY KEY (`id_supir`);
+
+--
+-- Indexes for table `testimoni`
+--
+ALTER TABLE `testimoni`
+  ADD PRIMARY KEY (`id_testimoni`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `historydatamobilyangdihapus`
+--
+ALTER TABLE `historydatamobilyangdihapus`
+  MODIFY `id_historymobil` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  MODIFY `id_pemesanan` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supir`
+--
+ALTER TABLE `supir`
+  MODIFY `id_supir` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `testimoni`
+--
+ALTER TABLE `testimoni`
+  MODIFY `id_testimoni` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
