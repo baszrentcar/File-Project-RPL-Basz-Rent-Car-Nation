7348ad28fc94e30fa406da2b41561989f16e00a7
baszrentcar <80313322+baszrentcar@users.noreply.github.com>
2021-04-29 12:18:04 +0700

    Add files via upload

--- baszrent/Database/baszrentcar.sql
+++ baszrent/Database/baszrentcar.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2021 at 05:25 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- Generation Time: Apr 29, 2021 at 07:17 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -119,6 +120,27 @@ CREATE TABLE `pemilik_rental` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id` int(11) NOT NULL,
+  `level` varchar(15) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`id`, `level`, `username`, `password`) VALUES
+(1, 'customer', 'bapidz', '123'),
+(2, 'pemilik', 'albapit', '321');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `supir`
 --
 
@@ -190,6 +212,12 @@ ALTER TABLE `pemilik_rental`
   ADD PRIMARY KEY (`no_ktp`);
 
 --
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `supir`
 --
 ALTER TABLE `supir`
@@ -228,6 +256,12 @@ ALTER TABLE `pembayaran`
 --
 ALTER TABLE `pemesanan`
   MODIFY `id_pemesanan` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `supir`
