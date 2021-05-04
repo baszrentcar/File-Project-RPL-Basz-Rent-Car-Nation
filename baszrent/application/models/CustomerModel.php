7321b11e17ebb4edc1ef6df91d0734cefbb6722c
Bagas <62025719+bagasmillena@users.noreply.github.com>
2021-05-04 14:04:17 +0700

    Update CustomerModel.php
    komen

--- baszrent/application/models/CustomerModel.php
+++ baszrent/application/models/CustomerModel.php
@@ -2,12 +2,19 @@
 
 class CustomerModel extends CI_model
 {
+	public function getAllCustomer()
+	{
+		// get semua data customer dari db customer
+		$query = $this->db->query("SELECT * FROM costumer");
+		return $query->result_array();
+	}
+
 	public function addCustomer()
 	{
-		//use query builder to insert $data to table "customer"
+		//insert data customer ke db customer
 		$data = [
 			"no_ktp" => $this->input->post('no_ktp', true),
-			"nama" => $this->input->post('name', true),
+			"nama" => $this->input->post('nama', true),
 			"email" => $this->input->post('email', true),
 			"username" => $this->input->post('username', true),
 			"password" => $this->input->post('password', true),
@@ -17,14 +24,29 @@ class CustomerModel extends CI_model
 
 	public function getCustomerByUsername($uname)
 	{
-		//get data account based on id 
+		//get data customer berdasarkan username
 		$query = $this->db->query("SELECT * FROM costumer WHERE username = '" . $uname . "'");
+		return $query->row_array();
+	}
+
+	public function getCustomerByKTP($no_ktp)
+	{
+		//get data customer berdasarkan no. KTP
+		$query = $this->db->query("SELECT * FROM costumer WHERE no_ktp = '" . $no_ktp . "'");
 		return $query->row_array();
 	}
 
 	public function editCustomer($username, $data)
 	{
+		//edita data customer berdasarkan username
 		$this->db->where('username', $username);
+		$this->db->update('costumer', $data);
+	}
+
+	public function editCustomerByKTP($no_ktp, $data)
+	{
+		//edita data customer berdasarkan no_ktp
+		$this->db->where('no_ktp', $no_ktp);
 		$this->db->update('costumer', $data);
 	}
 }
