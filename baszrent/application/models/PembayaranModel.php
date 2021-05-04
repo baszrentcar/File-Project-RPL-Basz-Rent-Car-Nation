51fc5b81627ebff844af54699af22eb3daf05753
Arya Kartawijaya <52090483+fenrilius@users.noreply.github.com>
2021-05-04 14:10:27 +0700

    menambahkan comment berisi penjelasan code

--- baszrent/application/models/PembayaranModel.php
+++ baszrent/application/models/PembayaranModel.php
@@ -4,7 +4,7 @@ class PembayaranModel extends CI_model
 {
     public function addPembayaran()
     {
-        // //use query builder to insert $data to table "pembayaran"
+        //query untuk menambahkan data pembayaran baru di database
         $data = [
             "id_pembayaran" => $this->input->post('id_pembayaran', true),
             "total_harga" => $this->input->post('total_harga', true),
@@ -15,7 +15,7 @@ class PembayaranModel extends CI_model
 
     public function getPembayaranById($id_pembayaran)
     {
-        //get data  based on id 
+        //mengambil/memanggil data pembayaran berdasarkan id pembayaran pada database
         $query = $this->db->query("SELECT * FROM mobil WHERE id_pembayaran = '" . $id_pembayaran . "'");
         return $query->row_array();
     }
