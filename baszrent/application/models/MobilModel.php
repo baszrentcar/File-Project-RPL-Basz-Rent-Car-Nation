6fc0ad11b1c7c8b1d590636c1e014ffc2468a254
Arya Kartawijaya <52090483+fenrilius@users.noreply.github.com>
2021-05-04 13:59:55 +0700

    menambahkan comment berisi penjelasan code

--- baszrent/application/models/MobilModel.php
+++ baszrent/application/models/MobilModel.php
@@ -4,37 +4,34 @@ class MobilModel extends CI_model
 {
     public function getAllMobil()
     {
-        // get all data
+        // mengambil/memanggil semua data mobil yang berada di database
         $query = $this->db->query("SELECT * FROM mobil");
         return $query->result_array();
     }
 
     public function addMobil($data)
     {
+        // menambahkan data baru mobil pada database
         $this->db->insert('mobil', $data);
     }
 
     public function getMobilById($id_mobil)
     {
-        //get data mobil based on id 
+        //mengambil/memanggil data mobil berdasarkan id mobil pada database
         $query = $this->db->query("SELECT * FROM mobil WHERE id_mobil = '" . $id_mobil . "'");
         return $query->row_array();
     }
 
     public function editMobil($id_mobil, $data)
     {
+        //mengedit data mobil berdasarkan id mobil yang dipilih
         $this->db->where('id_mobil', $id_mobil);
         $this->db->update('mobil', $data);
     }
 
     public function deleteMobil($id_mobil)
     {
+        //menghapus data mobil pada database berdasarkan idnya
         $this->db->delete('mobil', ['id_mobil' => $id_mobil]);
-    }
-
-    public function countId($id)
-    {
-        $query = $this->db->query("SELECT * FROM mobil WHERE id_mobil LIKE '" . $id . "%'");
-        return $query->num_rows();
     }
 }
