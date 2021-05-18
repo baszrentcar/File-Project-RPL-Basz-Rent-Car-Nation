7e0d32a1ff8e1f38427da9ea5e89239896d909ee
Arya Kartawijaya <52090483+fenrilius@users.noreply.github.com>
2021-05-18 14:22:36 +0700

    nambahin navbar

--- baszrent/application/views/templates/header.php
+++ baszrent/application/views/templates/header.php
@@ -20,6 +20,29 @@
     <link rel="stylesheet" href="<?= base_url(); ?>assets/css/styles.css">
 
     <title><?php echo $judul ?></title>
+	    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+        <!--<img class="navbar-brand" height="70px" src="<?php echo base_url(); ?>assets/images/logo/2.png">-->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="<?= base_url('MobilController'); ?>">Home</a>
+                    <a class="nav-item nav-link" href="<?= base_url('MobilController'); ?>">Pemesanan</a>
+                    <a class="nav-item nav-link" href="<?= base_url('MobilController'); ?>">Pembayaran</a>
+                    <a class="nav-item nav-link" href="<?= base_url('MobilController'); ?>">Testimoni</a>
+                </div>
+            </div>
+            <div class="text-right" class="collapse navbar-collapse">
+                <?php if(isset($user['fullname'])) { ?>
+                    <a class="btn navbar-dark my-2 my-sm-0" href="<?= base_url('HomeController/logout'); ?>">Logout</a>
+                <?php } else { ?>
+                    <a class="btn navbar-dark my-2 my-sm-0" href="<?= base_url('LoginController'); ?>">Login</a>
+                <?php }?>
+            </div>
+        </div>
+    </nav>
 </head>
 
 <body>
