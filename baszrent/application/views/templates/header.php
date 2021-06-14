afb9cc2dea3ab7c3eab5c743966566251987fb81
Bagas <62025719+bagasmillena@users.noreply.github.com>
2021-06-14 19:38:38 +0700

    Update header.php

--- baszrent/application/views/templates/header.php
+++ baszrent/application/views/templates/header.php
@@ -20,9 +20,9 @@
     <link rel="stylesheet" href="<?= base_url(); ?>assets/css/styles.css">
 
     <title><?php echo $judul ?></title>
-	    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-        <!--<img class="navbar-brand" height="70px" src="<?php echo base_url(); ?>assets/images/logo/2.png">-->
+            <!--<img class="navbar-brand" height="70px" src="<?php echo base_url(); ?>assets/images/logo/2.png">-->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -31,15 +31,15 @@
                     <a class="nav-item nav-link" href="<?= base_url('MobilController'); ?>">Home</a>
                     <a class="nav-item nav-link" href="<?= base_url('MobilController'); ?>">Pemesanan</a>
                     <a class="nav-item nav-link" href="<?= base_url('MobilController'); ?>">Pembayaran</a>
-                    <a class="nav-item nav-link" href="<?= base_url('MobilController'); ?>">Testimoni</a>
+                    <a class="nav-item nav-link" href="<?= base_url('TestimoniController'); ?>">Testimoni</a>
                 </div>
             </div>
             <div class="text-right" class="collapse navbar-collapse">
-                <?php if(isset($user['fullname'])) { ?>
+                <?php if (isset($user)) { ?>
                     <a class="btn navbar-dark my-2 my-sm-0" href="<?= base_url('HomeController/logout'); ?>">Logout</a>
                 <?php } else { ?>
                     <a class="btn navbar-dark my-2 my-sm-0" href="<?= base_url('LoginController'); ?>">Login</a>
-                <?php }?>
+                <?php } ?>
             </div>
         </div>
     </nav>
