93cea5a5672592287fbe726cae9fa3cd7c6c7c11
Arya Kartawijaya <52090483+fenrilius@users.noreply.github.com>
2021-06-14 19:49:10 +0700

    add view detail_pemesanan

---
+++ baszrent/application/views/detail_pemesanan.php
@@ -0,0 +1,157 @@
+<html>
+
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    .path_judul{
+        margin-top: 80px;
+        margin-left: 80px;
+    }
+    .input_tgl {
+        width: 35%;
+        padding-bottom: 10px;
+        padding-top: 10px;
+        padding-left: 10px;
+    }
+    label{
+        margin-top: 32px;
+    }
+    .tanggal{
+        margin-top: 60px;
+        margin-left: 120px;
+    }
+
+    p{
+        margin-top: 20px;
+        color: #8E93A3;
+        font: Roboto;
+        font-size: 18px;
+    }
+    .kirim_kembali{
+        margin-top: 40px;
+    }
+    .psn{
+        color: white;
+        background-color: #FF9900;
+        border: 0;
+        width: 200px;
+    }
+    .container-filter{
+        float: right;
+        border: 1px solid #C6CBDA;
+        margin-right: 120px;
+        margin-top: 50px;
+        width: 35%;
+        position: relative;
+        margin-bottom: 200px;
+    }
+    .hero{
+        text-align: left;
+    }
+    .value{
+        float: right;
+        white-space: inline;
+    }
+    .line{
+        white-space: nowrap;
+    }
+    .garis{
+        height: 1px;
+        font-size: 0%;
+        margin-left: 8px;
+        margin-right: 8px;
+        margin-top: 14px;
+        margin-bottom: 14px;
+        color: white;
+        background-color: #C4C4C4;
+    }
+    .tulisan{
+        margin-right: 60%;
+    }
+    .tulisan2{
+        border: 1px solid #C6CBDA;
+        margin-top: 20px;
+        padding: 2%;
+        margin-right: 60%;
+        margin-bottom: 80px;
+        padding-bottom: 10%;
+    }
+    h7{
+        color: #8E93A3;
+        float: right;
+    }
+    h6{
+        float: right;
+    }
+    .label-tulisan{
+        margin-top: 16px;
+        color: #8E93A3;
+    }
+    .sub-hero{
+        font-size: 20px;
+    }
+    .card{
+        margin-top: 40px;
+        margin-left: 120px;
+        margin-right: 120px;
+    }
+ 
+    </style>
+    
+<body>
+    <h1 style="background-color: #010F35;">a</h1>
+    <h4 class="path_judul">Pemesanan / history pemesanan / <b>Detail</b></h4>
+    <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+    <h4 class="card-title" class="hero"><b>"value nama mobil"</b></h4>
+        <div class="tulisan">
+            <p class="sub-hero"><b></b></p>
+            <span class="label-tulisan">Harga Sewa Mobil :</span>
+            <h7>"value Harga Sewa Mobil"</h7>
+            <br>
+            <span class="label-tulisan">Warna : </span>
+            <h7>"value Warna"</h7>
+            <br>
+            <span class="label-tulisan">Plat Nomor : </span>
+            <h7>"value Plat Nomor"</h7>
+            <br>
+            <span class="label-tulisan">Deskiprisi : </span>
+            <h7>"value Deskiprisi"</h7>
+            <br>
+            <span class="label-tulisan">Status : </span>
+            <h7>"value Status"</h7>
+            <br>
+            <span class="label-tulisan">Penumpang : </span>
+            <h7>"value Penumpang"</h7>
+            
+        </div>
+        <div class="card-body2">
+            <div class="tulisan2">
+                <h4 class="hero"><b>Sewa Mobil</b></h4>
+                <p class="sub-hero"><b>"nama mobil"</b></p>
+                <span class="label-tulisan">Tanggal mulai</span>
+                <h7>"value tanggal mulai"</h7>
+                <br>
+                <span class="label-tulisan">Tanggal Selesai</span>
+                <h7>"value Tanggal Selesai"</h7>
+                <h1 class="garis">.</h1>
+                <p class="sub-hero"><b>Harga</b></p>
+                <span class="label-tulisan">1 hari x 1 mobil</span>
+                <h7>"value 1 hari x 1 mobil"</h7>
+                <br>
+                <span class="label-tulisan">Supir </span>
+                <h7>"Supir ?"</h7>
+                <h1 class="garis">.</h1>
+                <br>
+                <span class="label-tulisan">Total Pembayaran</span>
+                <h6><b>"value Total Pembayaran"</b></h6>
+            </div>
+</body>
+
+</html>
