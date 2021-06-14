151747b29190814551e78546388980b375914cd1
Arya Kartawijaya <52090483+fenrilius@users.noreply.github.com>
2021-06-14 21:29:56 +0700

    add view lihat_mobil

---
+++ baszrent/application/views/lihat_mobil.php
@@ -0,0 +1,144 @@
+<html>
+
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    .path_judul{
+        margin-top: 80px;
+        margin-left: 80px;
+    }
+    .input_tgl {
+        width: 35%;
+        padding-bottom: 10px;
+        padding-top: 10px;
+        padding-left: 10px;
+    }
+    label{
+        margin-top: 32px;
+    }
+    .tanggal{
+        margin-top: 60px;
+        margin-left: 120px;
+    }
+
+    p{
+        margin-top: 20px;
+        color: #8E93A3;
+        font: Roboto;
+        font-size: 18px;
+    }
+    .kirim_kembali{
+        margin-top: 40px;
+    }
+    .psn{
+        color: white;
+        background-color: #FF9900;
+        border: 0;
+        width: 100px;
+    }
+    .container-filter{
+        float: right;
+        border: 1px solid #C6CBDA;
+        margin-right: 120px;
+        margin-top: 50px;
+        width: 35%;
+        position: relative;
+        margin-bottom: 200px;
+    }
+    .hero{
+        text-align: left;
+    }
+    .value{
+        float: right;
+        white-space: inline;
+    }
+    .line{
+        white-space: nowrap;
+    }
+    .garis{
+        height: 1px;
+        font-size: 0%;
+        margin-left: 8px;
+        margin-right: 8px;
+        margin-top: 14px;
+        margin-bottom: 14px;
+        color: white;
+        background-color: #C4C4C4;
+    }
+    .tulisan{
+        margin-right: 60%;
+    }
+    .tulisan2{
+        border: 1px solid #C6CBDA;
+        margin-top: 20px;
+        padding: 2%;
+        margin-right: 60%;
+        margin-bottom: 80px;
+        padding-bottom: 10%;
+    }
+    h7{
+        color: #8E93A3;
+        float: right;
+    }
+    h6{
+        float: right;
+    }
+    .label-tulisan{
+        margin-top: 16px;
+        color: #8E93A3;
+    }
+    .sub-hero{
+        font-size: 20px;
+    }
+    .card{
+        position: sticky;
+        margin-top: 40px;
+        margin-left: 120px;
+        margin-right: 120px;
+    }
+    aside{
+        position: relative;
+        float: right;
+    }
+    </style>
+    
+<body>
+    <h1 style="background-color: #010F35;">a</h1>
+    <h4 class="path_judul">Home / <b>Lihat Mobil</b></h4>
+    <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+    <h4 class="card-title" class="hero"><b>"value nama mobil"</b></h4>
+    <div>
+        <aside>"value harga mobil/hari"</aside>
+        <br><br>
+        <aside><a href="" class="btn btn-primary psn"class="btn-psn">SEWA</a></aside>
+    </div>
+        <div class="tulisan">
+            <p class="sub-hero"><b></b></p>
+            <span class="label-tulisan">Harga Sewa Mobil :</span>
+            <h7>"value Harga Sewa Mobil"</h7>
+            <br>
+            <span class="label-tulisan">Warna : </span>
+            <h7>"value Warna"</h7>
+            <br>
+            <span class="label-tulisan">Plat Nomor : </span>
+            <h7>"value Plat Nomor"</h7>
+            <br>
+            <span class="label-tulisan">Deskiprisi : </span>
+            <h7>"value Deskiprisi"</h7>
+            <br>
+            <span class="label-tulisan">Status : </span>
+            <h7>"value Status"</h7>
+            <br>
+            <span class="label-tulisan">Penumpang : </span>
+            <h7>"value Penumpang"</h7>     
+        </div>
+</body>
+
+</html>
