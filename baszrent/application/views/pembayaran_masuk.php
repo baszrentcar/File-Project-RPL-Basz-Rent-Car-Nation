5c01ab379d363f80fdee9c95a90b3c4f42af0c83
Arya Kartawijaya <52090483+fenrilius@users.noreply.github.com>
2021-06-14 20:38:53 +0700

    add view pembayaran_masuk(pemilik)

---
+++ baszrent/application/views/pembayaran_masuk.php
@@ -0,0 +1,56 @@
+<html>
+
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    .path_judul{
+        margin-top: 80px;
+        margin-left: 80px;
+    }
+
+    .sub_judul{
+        margin-top: 40px;
+        margin-left: 80px;
+    }
+    .container{
+        margin-top: 40px;
+        margin-left: 80px;
+        margin-bottom: 120px;
+    }
+    </style>
+    
+<body>
+    <h1 style="background-color: #010F35;">a</h1>
+    <h4 class="path_judul">Pembayaran / <b>Pembayaran masuk</b></h4>
+    <br>
+    <h7 class="sub_judul">Menampilkan Tabel Pembayaran yang masuk</h7>
+
+    <div class="container">           
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>Id Pesanan</th>
+            <th>Id Pembayaran</th>
+            <th>Nama</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>01</td>
+            <td>01</td>
+            <td>John</td>
+            <td>value status = proses/selesai</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+
+</body>
+
+</html>
