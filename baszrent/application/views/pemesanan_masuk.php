eea568d8d8d94b46c167cebf85efefa42e246260
Arya Kartawijaya <52090483+fenrilius@users.noreply.github.com>
2021-06-14 20:31:39 +0700

    add view pemesanan_masuk

---
+++ baszrent/application/views/pemesanan_masuk.php
@@ -0,0 +1,60 @@
+<html>
+
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    .path_judul{
+        margin-top: 80px;
+        margin-left: 80px;
+    }
+
+    .sub_judul{
+        margin-top: 40px;
+        margin-left: 80px;
+    }
+    .container{
+        margin-top: 40px;
+        margin-left: 80px;
+        margin-bottom: 120px;
+    }
+    </style>
+    
+<body>
+    <h1 style="background-color: #010F35;">a</h1>
+    <h4 class="path_judul">Pemesanan / <b>Pemesanan masuk</b></h4>
+    <br>
+    <h7 class="sub_judul">Menampilkan Tabel Pesanan Yang Masuk</h7>
+
+    <div class="container">           
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>Id Pesanan</th>
+            <th>Nama Pemesan</th>
+            <th>Mobil</th>
+            <th>Aksi</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+            <td>
+            <a href="" class="btn btn-primary psn"class="btn-psn">Terima</a> 
+            <a href="" class="btn btn-primary psn"class="btn-psn">Lihat</a>
+            <a href="" class="btn btn-primary psn"class="btn-psn">Hapus</a>
+        </td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+
+</body>
+
+</html>
