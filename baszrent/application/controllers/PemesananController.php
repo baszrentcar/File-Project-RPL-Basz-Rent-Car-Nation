bbb6afd65cbe4954014365fad4a43c892ec965fb
Bagas <62025719+bagasmillena@users.noreply.github.com>
2021-06-14 19:45:36 +0700

    Update PemesananController.php

--- baszrent/application/controllers/PemesananController.php
+++ baszrent/application/controllers/PemesananController.php
@@ -7,6 +7,8 @@ class PemesananController extends CI_Controller
         parent::__construct();
         // load 
         $this->load->model('PemesananModel');
+        $this->load->library('form_validation');
+        $this->load->library('session');
     }
 
     public function index()
@@ -15,5 +17,22 @@ class PemesananController extends CI_Controller
         $data['user'] = $this->session->userdata('user');
 
         $this->load->view('Pemesanan/index.php', $data);
+    }
+
+    public function pesan($id_mobil)
+    {
+        $data['user'] = $this->session->userdata('user');
+        $data['judul'] = 'Pemesanan';
+        $add = [
+            "id_pemesanan" => '',
+            "id_supir" => '',
+            "id_mobil" => $id_mobil,
+            "no_ktp" => $data['user']['no_ktp'],
+            "tanggal_pesan" => $this->input->post('tanggal_pesan', true),
+            "tanggal_kembali" => $this->input->post('tanggal_kembali', true),
+
+        ];
+        $this->PemesananModel->addPemesanan($add);
+        redirect('PembayaranController');
     }
 }
