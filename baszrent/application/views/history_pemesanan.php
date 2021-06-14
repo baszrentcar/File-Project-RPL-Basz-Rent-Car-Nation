1a6db6f59d1497cbae1d3dbb67817484dc24f5d5
Arya Kartawijaya <52090483+fenrilius@users.noreply.github.com>
2021-06-14 17:06:11 +0700

    add view history_pemesanan(user)

---
+++ baszrent/application/views/history_pemesanan.php
@@ -0,0 +1,66 @@
+<html>
+
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    .path_judul{
+        margin-top: 80px;
+        margin-left: 80px;
+    }
+    .konten{
+        margin-top: 40px;
+        margin-left: 120px;
+        margin-bottom: 420px;
+    }
+    .card{
+        margin-top: 24px;
+        margin-right: 120px;
+        padding-bottom: 40px;
+    }
+    .btn-primary{
+        float: right;
+        margin-right: 40px;
+    }
+    .card-text2{
+        float: right;
+        margin-right: 40px;
+    }
+    .card-title{
+        position: absolute;
+    }
+    </style>
+    
+<body>
+    <h1 style="background-color: #010F35;">a</h1>
+    <h4 class="path_judul">Pemesanan / <b>History Pemesanan</b></h4>
+    <br>
+    <div class="konten">
+        <h4>History Mobil Yang Telah Disewa Pengguna</h4>
+        <h7>Menampilkan list mobil</h7>
+        <div class="listmobil">
+            <div class="card ">
+                <div class="row g-0">
+                    <div class="col-md-2">
+                    <img src="..." alt="...">
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text2">Status : <b>value status</b></p>
+                        <br><br>
+                        <a href="" class="btn btn-primary psn"class="btn-psn">Detail</a> 
+                        <h5 class="card-title">value nama mobil</h5>
+                        <br><br>
+                        <p class="card-text">Penumpang : <b>value penumpang</b></p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>
+</body>
+
+</html>
