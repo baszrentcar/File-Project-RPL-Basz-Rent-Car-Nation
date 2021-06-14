126057521926001c7209c677f91fb99d7f91eafa
Bagas <62025719+bagasmillena@users.noreply.github.com>
2021-06-14 19:28:23 +0700

    Update LoginController.php

--- baszrent/application/controllers/LoginController.php
+++ baszrent/application/controllers/LoginController.php
@@ -6,31 +6,27 @@ class LoginController extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('LoginModel');				# Model untuk Login
+        $this->load->model('CustomerModel');                # Model untuk Login
         $this->load->library('session');
     }
 
     public function index()
     {
+
         $this->load->view('view_login');
     }
 
-    public function aksi_login() {
-        $data['username'] = $this->input->post('username'); 		# melakukan post terhadap username
-   	    $data['password'] = $this->input->post('password'); 		# melakukan post terhadap password
-		 
-	    if($this->LoginModel->login($data)) {			            # Mengakses fungsi login pada Login Model
-            $data = $this->LoginModel->getProfile($data['username']);	# Untuk mendapatkan semua data di database untuk dibandingkan
-                if($data['level'] == 'customer') {                      # Apabila data yang diinputkan adalah akun milik pelanggan, maka akan masuk ke tampilan Home(test)
-                    redirect('MobilController');				
-                } else if ($data['level'] == "pemilik") {              	# Apabila data yang diinputkan adalah akun milik pemilik rental, maka akan masuk ke tampilan Signup(test)
-                    redirect('MobilController');
-                }
-   		    $this->session->set_userdata('username', $this->input->post('username')); 	# Menambah variabel session untuk username ke dalam server
-    	    $this->session->set_userdata('password', $this->input->post('password'));	# Menambah variabel session untuk password ke dalam server
-		    } 
-	    else {
-            redirect('LoginController', $data);				# Apabila data tidak ditemukan maka akan load kembali tampilan Login
+    public function aksi_login()
+    {
+        $uname = $this->input->post('username');         # melakukan post terhadap username
+        $pass = $this->input->post('password');         # melakukan post terhadap password
+
+        if ($this->CustomerModel->login($uname, $pass) == true) {            # Mengakses fungsi login pada Login Model
+            $data = $this->CustomerModel->getCustomerByUsername($uname);    # Untuk mendapatkan semua data di database untuk dibandingkan
+            $this->session->set_userdata('user', $data);
+            redirect('MobilController');
+        } else {
+            redirect('LoginController');                # Apabila data tidak ditemukan maka akan load kembali tampilan Login
         }
     }
 }
